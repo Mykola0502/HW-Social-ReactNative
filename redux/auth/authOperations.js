bea6719b6ee9bbcb762daca72877e506508c8049
Mykola Saliuk <haker0502@ukr.net>
2023-07-16 20:17:33 +0300

    Метод updateProfile, оновлення профайлу користувача (зберігає login в стейт)

--- redux/auth/authOperations.js
+++ redux/auth/authOperations.js
@@ -11,13 +11,34 @@ export const authSignUpUser =
   ({ email, password, login }) =>
   async (dispatch, getState) => {
     try {
-      const { user } = await createUserWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-      console.log("user", user);
-      dispatch(authSlice.actions.updateUserProfile({ userId: user.uid }));
+      await createUserWithEmailAndPassword(auth, email, password);
+
+      const user = await auth.currentUser;
+
+      if (user) {
+        try {
+          await updateProfile(user, {
+            displayName: login,
+          });
+
+          const { uid, displayName } = await auth.currentUser;
+
+          // console.log("displayName", displayName);
+
+          // console.log("user", user);
+
+          dispatch(
+            authSlice.actions.updateUserProfile({
+              userId: uid,
+              login: displayName,
+            })
+          );
+        } catch (error) {
+          console.log("updateError", error);
+          console.log("updateError.message", error.message);
+          throw error;
+        }
+      }
     } catch (error) {
       console.log("error", error);
       console.log("error.message", error.message);
